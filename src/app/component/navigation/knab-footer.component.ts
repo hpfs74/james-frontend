b6924769413b2aba2f758eeb30217b6fb70e5015
Matteo Salvestrini <matteo.salvestrini@aegon.nl>
2017-02-20 14:01:51 +0100

    refactor(component): generic refact

--- src/app/component/navigation/knab-footer.component.ts
+++ src/app/component/navigation/knab-footer.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+/**
+ * @description
+ * KnabFooterComponent handle to footer of the page
+ */
 @Component({
   selector: 'knab-footer',
   templateUrl: 'knab-footer.component.html',
 })
 export class KnabFooterComponent {
+  @Input() SocialIcons: boolean = false;
+
 }
