486dee2b039db71963d5f22d15ef4779b34cc7a2
Matteo Salvestrini <matteo.salvestrini@aegon.nl>
2017-02-20 14:01:27 +0100

    feat(model): add model for car and price

--- src/app/models/price.d.ts
+++ src/app/models/price.d.ts
@@ -1,6 +1,15 @@
+/**
+ * @description
+ * Price table element to be used with
+ * knab-price-table o knab-price-table-item
+ */
 export class Price {
     Header: string;
     Price: number;
+    /**
+     * @description
+     * List of features
+     */
     Features: Array<string>;
     Highlight: boolean;
 }
---
+++ src/app/models/vehicle.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @description
+ * Class to describe a vehicle data to be presented
+ * through the knab-vehicle.
+ */
+export class Vehicle {
+    Name: string;
+    Manufacturer: string;
+    Model: string;
+    Engine: string;
+    Transmission: string;
+    Year: Number;
+    AcquisitionValue: Number;
+    EstimatedValue: Number;
+}
