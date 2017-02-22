c8bd86071608349983fe10ad4310eeffdb36e275
Matteo Salvestrini <matteo.salvestrini@aegon.nl>
2017-02-22 16:46:20 +0100

    feat(vehicle): add component to show vehicle box

--- src/app/component/ki-vehicle-info/vehicle-info.component.ts
+++ src/app/component/ki-vehicle-info/vehicle-info.component.ts
@@ -1,3 +1,44 @@
-/**
- * Created by matteo on 2/22/17.
- */
+import { Component, Input } from '@angular/core';
+import { Vehicle } from '../../models/vehicle';
+
+@Component({
+  selector: 'ki-vehicle-info',
+  template: `
+    <div class="ki-block-round">
+      <div class="cx-row">
+        <div class="cx-sm-col-6">Merk</div>
+        <div class="cx-sm-col-6">{{ vehicle.manufacturer }}</div>
+      </div>  
+      <div class="cx-row">
+        <div class="cx-sm-col-6">Model</div>
+        <div class="cx-sm-col-6">{{ vehicle.model }}</div>
+      </div>
+      <div class="cx-row">
+        <div class="cx-sm-col-6">Brandstof</div>
+        <div class="cx-sm-col-6">{{ vehicle.engine }}</div>
+      </div>
+      <div class="cx-row">
+        <div class="cx-sm-col-6">Transmissie</div>
+        <div class="cx-sm-col-6">{{ vehicle.transmission }}</div>
+      </div>
+      <div class="cx-row">
+        <div class="cx-col-sm-6">Bouwjaar</div>
+        <div class="cx-col-sm-6">{{ vehicle.year }}</div>
+      </div>      
+      <div class="cx-row">
+        <div class="cx-col-sm-6">Aanschafwaarde</div>
+        <div class="cx-col-sm-6">{{ vehicle.acquisitionValue }}</div>
+      </div>
+      <div class="cx-row">
+        <div class="cx-col-sm-6">Geschatte waarde</div>
+        <div class="cx-col-sm-6">{{ vehicle.estimatedValue }}</div>
+      </div>
+  </div>`
+})
+
+export class VehicleInfoComponent {
+
+  @Input() vehicle: Vehicle;
+  @Input() comment: string;
+
+}
