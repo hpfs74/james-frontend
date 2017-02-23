ab07793a0432ffeeafea262c8ec8944d40f14714
Matteo Salvestrini <matteo.salvestrini@aegon.nl>
2017-02-23 14:43:32 +0100

    refactor(component): use service in app boot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Price, Nav, Feature } from './models';
 @Component({
   selector: 'ki-app',
   templateUrl: 'app.component.html',
+  providers: [ FeatureService ]
 })
 export class AppComponent implements OnInit {
   name = 'Angular';
@@ -13,24 +14,24 @@ export class AppComponent implements OnInit {
   topMenu: Array<Nav>;
   features: Array<Feature>;
 
+  constructor(private featureService:FeatureService ) {
+
+  }
+
   ngOnInit() {
     this.prices = [
-      { header: 'WA', price: 18.90, features: ['Feat1', 'Feat2'] },
-      { header: 'WA + Casco', price: 22.90, features: ['Feat1', 'Feat2', 'Feat3'], highlight: true },
-      { header: 'WA limited', price: 30.19, features: ['Feat1', 'Feat2', 'Feat3', 'Feat4', 'Feat5'] },
+      {header: 'WA', price: 18.90, features: ['Feat1', 'Feat2']},
+      {header: 'WA + Casco', price: 22.90, features: ['Feat1', 'Feat2', 'Feat3'], highlight: true},
+      {header: 'WA limited', price: 30.19, features: ['Feat1', 'Feat2', 'Feat3', 'Feat4', 'Feat5']},
     ];
 
     this.topMenu = [
-      { id: 'menu-overview', title: 'Overzicht' },
-      { id: 'menu-account', title: 'Mijn account' },
-      { id: 'menu-faq', title: 'FAQ' },
-      { id: 'menu-about', title: 'Over ons' },
+      {id: 'menu-overview', title: 'Overzicht'},
+      {id: 'menu-account', title: 'Mijn account'},
+      {id: 'menu-faq', title: 'FAQ'},
+      {id: 'menu-about', title: 'Over ons'},
     ];
 
-    this.features = [
-      { title: 'Gemak', description: 'Een digitaal overzicht van al je schadeverzekeringen van elke verzekeraar' },
-      { title: 'Proactief', description: 'Altijd direct op de hoogte van de beste deals' },
-      { title: 'Onafhankelijk', description: 'Alle vergelijkingen zijn onafhankelijk. Geen verzekeraar heeft voorrang' }
-    ];
+    this.features = this.featureService.getFeatures();
   }
 }
