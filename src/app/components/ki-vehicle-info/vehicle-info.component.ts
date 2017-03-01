832fa2ae32ba6d4833a6bb7b77e0b8e7f67b01ba
Matteo Salvestrini <matteo.salvestrini@aegon.nl>
2017-03-01 11:56:17 +0100

    fix(component): style to car component

--- src/app/components/ki-vehicle-info/vehicle-info.component.ts
+++ src/app/components/ki-vehicle-info/vehicle-info.component.ts
@@ -7,6 +7,11 @@ import { Vehicle } from '../../models/vehicle';
   template: `
   <div class="ki-block-round">      
     <div class="cx-container-fluid">
+    
+      <div class="cx-row">
+        <div class="cx-col-sm-12">Molto bello! Mooie auto die <b>{{ vehicle.manufacturer }} {{ vehicle.name }} {{ vehicle.model }}</b></div>
+      </div>  
+    
       <div class="cx-row">
         <div class="cx-col-sm-12">
           <img alt="vehicle image" border="0" 
@@ -15,31 +20,31 @@ import { Vehicle } from '../../models/vehicle';
       </div>
       <div class="cx-row">
         <div class="cx-col-sm-6">Merk</div>
-        <div class="cx-col-sm-6">{{ vehicle.manufacturer }} {{ vehicle.name }}</div>
+        <div class="cx-col-sm-6 ki-block-round__cartext">{{ vehicle.manufacturer }} {{ vehicle.name }}</div>
       </div>
       <div class="cx-row">
         <div class="cx-col-sm-6">Model</div>
-        <div class="cx-col-sm-6">{{ vehicle.model }}</div>
+        <div class="cx-col-sm-6 ki-block-round__cartext">{{ vehicle.model }}</div>
       </div>
       <div class="cx-row" *ngIf="vehicle.engine">
         <div class="cx-col-sm-6">Brandstof</div>
-        <div class="cx-col-sm-6">{{ vehicle.engine }}</div>
+        <div class="cx-col-sm-6 ki-block-round__cartext">{{ vehicle.engine }}</div>
       </div>
       <div class="cx-row" *ngIf="vehicle.transmission">
         <div class="cx-col-sm-6">Transmissie</div>
-        <div class="cx-col-sm-6">{{ vehicle.transmission }}</div>
+        <div class="cx-col-sm-6 ki-block-round__cartext">{{ vehicle.transmission }}</div>
       </div>
       <div class="cx-row" *ngIf="vehicle.year">
         <div class="cx-col-sm-6">Bouwjaar</div>
-        <div class="cx-col-sm-6">{{ vehicle.year }}</div>
+        <div class="cx-col-sm-6 ki-block-round__cartext">{{ vehicle.year }}</div>
       </div>
       <div class="cx-row" *ngIf="vehicle.acquisitionValue">
         <div class="cx-col-sm-6">Aanschafwaarde</div>
-        <div class="cx-col-sm-6">{{ vehicle.acquisitionValue | currency }}</div>
+        <div class="cx-col-sm-6 ki-block-round__cartext">{{ vehicle.acquisitionValue | currency }}</div>
       </div>
       <div class="cx-row" *ngIf="vehicle.estimatedValue">
         <div class="cx-col-sm-6">Geschatte waarde</div>
-        <div class="cx-col-sm-6">{{ vehicle.estimatedValue | currency }}</div>
+        <div class="cx-col-sm-6 ki-block-round__cartext">{{ vehicle.estimatedValue | currency }}</div>
       </div>
       </div>
   </div>`
