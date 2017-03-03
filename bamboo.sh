2f9499dce090dfc09090b577d9fd4d201d90b868
Matteo Salvestrini <matteo.salvestrini@aegon.nl>
2017-03-03 10:04:40 +0100

    feat(cicd): bamboo script

--- bamboo.sh
+++ bamboo.sh
@@ -27,8 +27,8 @@ npm run build
 
 
 # Deployment
-export AWS_ACCESS_KEY_ID=AKIAIHJBGUS6V4RNM6PQ
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_DEFAULT_REGION=us-west-1
- 
-aws s3 cp dist/ s3://aws-website-knab-dev-0b8y4/${bamboo.buildResultKey}/ --recursive --include "*"
+#export AWS_ACCESS_KEY_ID=AKIAIHJBGUS6V4RNM6PQ
+#export AWS_SECRET_ACCESS_KEY=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
+#export AWS_DEFAULT_REGION=us-west-1
+# 
+#aws s3 cp dist/ s3://aws-website-knab-dev-0b8y4/${bamboo.buildResultKey}/ --recursive --include "*"
