bdd256c8a8d00bca3fe8c93207e762dd991344f6
Matteo Salvestrini <matteo.salvestrini@aegon.nl>
2017-03-03 12:43:06 +0100

    feat(cicd): bamboo script

--- bamboo.sh
+++ bamboo.sh
@@ -14,15 +14,11 @@ npm --version
 
 # We need to switch npm global packages location to space we have permission to access
 # required for linking dependencies
-mkdir -p "$(pwd)/vendor/node_modules"
-export NPM_CONFIG_PREFIX="$(pwd)/vendor/node_modules/.npm-global"
-export NPM_CONFIG_REGISTRY=https://swfactory.aegon.com/artifactory/api/npm/npm
+#mkdir -p "$(pwd)/vendor/node_modules"
+#export NPM_CONFIG_PREFIX="$(pwd)/vendor/node_modules/.npm-global"
+#export NPM_CONFIG_REGISTRY=https://swfactory.aegon.com/artifactory/api/npm/npm
 
-
-#npm config set prefix "$(pwd)/vendor/node_modules"
-#npm config set @cx:registry 
-
-npm install
+npm install --scripts-prepend-node-path=false
 npm run build
 
 
