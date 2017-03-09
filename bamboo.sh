93f17371e9ed7d57e1f42533abc15f3a14d6b70e
Matteo Salvestrini <matteo.salvestrini@aegon.nl>
2017-03-09 15:10:53 +0100

    fix(aws): restricted access to s3 buckets

--- bamboo.sh
+++ bamboo.sh
@@ -30,9 +30,3 @@ npm test
 
 echo "Build done"
 
-# Deployment
-export AWS_ACCESS_KEY_ID=AKIAIHJBGUS6V4RNM6PQ
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_DEFAULT_REGION=us-west-1
-
-aws s3 cp dist/ s3://aws-website-knab-dev-0b8y4/ --recursive --include "*"
