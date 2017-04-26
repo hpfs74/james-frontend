4f0d0553d01088c3e05c4d2a442f45e9262250bf
Matteo Salvestrini <matteo.s@hcl.com>
2017-04-26 11:43:46 +0200

    refactor(login): add test component for login
    add test component stub for login component

---
+++ src/app/pages/login/login.component.spec.ts
@@ -0,0 +1,33 @@
+import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('Component: Navbar', () => {
+  let comp: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [LoginComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    comp = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should display error message on wrong email syntax');
+  it('should display error message on invalid login');
+  it('should disable submit button until form is valid');
+  it('should notify success on correct login data');
+  it('should display error on invalid login data');
+});
