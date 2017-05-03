97693b307e4f8da2a208df919317483821dadef2
Matteo Salvestrini <matteo.s@hcl.com>
2017-05-03 14:39:36 +0200

    refactor(authservice): reomve unused files
    Signed-off-by: Matteo Salvestrini <matteo.s@hcl.com>

--- src/app/models/auth.ts
+++ src/app/models/auth.ts
@@ -1,16 +1,16 @@
-
-import { Observable } from 'rxjs/Observable';
-import { User } from './user';
-/**
- *
- */
-export interface IAuthService {
-  getUserProfile() : Observable<User>;
-  signIn(email: string, password: string) : Observable<AuthToken>;
-  isActive(email : string);
-  resendActivation(email:string);
-  forgotPassword(email:string);
-}
+//
+// import { Observable } from 'rxjs/Observable';
+// import { User } from './user';
+// /**
+//  *
+//  */
+// export interface IAuthService {
+//   getUserProfile() : Observable<User>;
+//   signIn(email: string, password: string) : Observable<AuthToken>;
+//   isActive(email : string);
+//   resendActivation(email:string);
+//   forgotPassword(email:string);
+// }
 
 export class AuthToken {
   access_token: string;
