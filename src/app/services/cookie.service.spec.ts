f9bf143cc90b160db94456e15d5242c510f4f440
Matteo Salvestrini <matteo.s@hcl.com>
2017-05-08 16:39:37 +0200

    refactor(cookies): add improvement on testing code

--- src/app/services/cookie.service.spec.ts
+++ src/app/services/cookie.service.spec.ts
@@ -1,14 +1,14 @@
 
 import { CookieService }  from './cookie.service';
 
-describe('Service: CookieService', function () {
+describe('Service: CookieService', () => {
   let service: CookieService;
 
   beforeEach( () => {
     service = new CookieService();
   });
 
-  it('all method should be defined', function() {
+  it('all method should be defined', () => {
     expect(service).toBeDefined();
     expect(service.check).toBeDefined();
     expect(service.delete).toBeDefined();
@@ -18,17 +18,17 @@ describe('Service: CookieService', function () {
     expect(service.set).toBeDefined();
   });
 
-  describe('Cookie check methods', function () {
-    it('should return true if cookie is present', function () {
+  describe('Cookie check methods', () => {
+    it('should return true if cookie is present', () => {
       service.set('test', '1234');
       expect(service.check('test')).toBeTruthy();
     });
 
-    it('should return false if cookie is not preset', function () {
+    it('should return false if cookie is not preset', () => {
       expect(service.check('notexisting')).toBeFalsy();
     });
 
-    it('should be able to get the list of all the cookies', function () {
+    it('should be able to get the list of all the cookies', () => {
       service.set('cook1', '1234');
       service.set('cook2', '5678');
       let ret = service.getAll();
@@ -36,35 +36,35 @@ describe('Service: CookieService', function () {
     });
   });
 
-  describe('Cookie manipulation method', function() {
+  describe('Cookie manipulation method', () => {
 
-    it('should return empty string requesting not existing cookie', function() {
+    it('should return empty string requesting not existing cookie', () => {
       expect(service.get('notexisting')).toBe('');
     });
 
-    it('should set the correct expire date in days', function() {
+    it('should set the correct expire date in days', () => {
       pending();
     });
 
-    it('should set the correct expire date with a date object', function () {
+    it('should set the correct expire date with a date object', () => {
       pending();
     });
 
-    it('should be able to add a new cookie', function() {
+    it('should be able to add a new cookie', () => {
       service.set('cook3', '9999');
-      var ret = service.get('cook3');
+      let ret = service.get('cook3');
       expect(ret).toBe('9999');
     });
 
-    it('should be able to remove a cookie', function() {
+    it('should be able to remove a cookie', () => {
       service.set('cook4', '1111');
       service.delete('cook4');
       expect(service.check('cook4')).toBeFalsy();
     });
 
-    it ('should be able delete all cookie', function() {
+    it ('should be able delete all cookie', () => {
       service.deleteAll();
-      var ret = service.getAll();
+      let ret = service.getAll();
 
       expect(Object.keys(ret).length).toBe(0);
     });
