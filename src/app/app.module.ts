5086bfc4abe40d2ed18ce807fc2c2ce19706fa77
Thomas Grendel <thomas-g@hcl.com>
2017-05-23 14:14:39 +0200

    chore(config): use mock api by default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import '../styles/styles.scss';
 
 // Needed because app initializer doesn't work with anonymous function
 export function ConfigLoader(configService: ConfigService) {
+<<<<<<< HEAD
   let configFile = './config/api/config.mock.json';
 
   if (process.env.ENV === 'test') {
@@ -44,6 +45,9 @@ export function ConfigLoader(configService: ConfigService) {
   }
 
   return () => configService.load(configFile);
+=======
+  return () => configService.load('./config/api/config.mock.json');
+>>>>>>> 69976fd... chore(config): use mock api by default
 }
 
 // !! Ensure AppRoutingModule is always imported last
