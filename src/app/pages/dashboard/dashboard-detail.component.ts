3b843356bcfdec44440d56c43fcd2e734d36a98b
Matteo Salvestrini <matteo.s@hcl.com>
2017-05-26 10:11:55 +0200

    refactor(dashboard): add dashboard detail component
    Signed-off-by: Matteo Salvestrini <matteo.s@hcl.com>

--- src/app/pages/dashboard/dashboard-detail.component.ts
+++ src/app/pages/dashboard/dashboard-detail.component.ts
@@ -5,21 +5,12 @@ import { ChatStreamService } from '../../components/knx-chat-stream/chat-stream.
 import { AssistantConfig } from './../../models/assistant';
 import { ChatMessage } from './../../components/knx-chat-stream/chat-message';
 import { Profile } from '../../models';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  template: `
-    <div class="container knx-container-dashboard">
-      <div class="row">
-        <div class="col-md-8">
-          <h2>Maak een keuze</h2>
-          
-        </div>
-        <div class="col-md-4">
-          <knx-chat-stream [options]="chatConfig" [messages]="chatMessages"></knx-chat-stream>
-        </div>
-      </div>
-    </div>
-  `
+  selector: 'knab-dashboard-detail',
+  templateUrl: 'dashboard-detail.component.html',
+  styleUrls: ['dashboard-detail.component.scss'],
 })
 export class DashboardDetailComponent implements OnInit, AfterViewInit {
   insuranceType: string;
@@ -29,15 +20,18 @@ export class DashboardDetailComponent implements OnInit, AfterViewInit {
 
   constructor(private profileService: ProfileService,
               private assistantService: AssistantService,
-              private chatNotifierService: ChatStreamService) {
+              private chatNotifierService: ChatStreamService,
+              private route: ActivatedRoute) {
+
     this.chatConfig = assistantService.config;
     this.chatConfig.avatar.title = 'Expert verzekeringen';
   }
 
   ngOnInit() {
 
-    // TODO: get the insurance type parameter
-    this.insuranceType = 'auto';
+    this.route.data.subscribe(x=> {
+      this.insuranceType = x.insuranceType;
+    });
 
     this.chatNotifierService.addMessage$.subscribe(
       message => {
