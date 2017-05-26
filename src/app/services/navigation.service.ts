30cdcfed30e43d6f358b0026830f0a75c233525f
Thomas Grendel <thomas-g@hcl.com>
2017-05-26 09:25:07 +0200

    chore(navbar): reformat on newline

--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -8,7 +8,10 @@ export class NavigationService {
     return [
       { id: 'menu-dashboard', title: 'Verzekeringen', routePath: '/', menuType: NavItemType.RIGHT },
       { id: 'menu-faq', title: 'FAQ', routePath: 'faq', menuType: NavItemType.RIGHT },
-      { id: 'menu-phone', icon: 'knx-icon-whatsapp', title: '020-303 1680', routePath: '', menuType: NavItemType.RIGHT, url: 'tel:0203031680'}
+      {
+        id: 'menu-phone', icon: 'knx-icon-whatsapp', title: '020-303 1680', routePath: '',
+        menuType: NavItemType.RIGHT, url: 'tel:0203031680'
+      }
     ];
   }
 }
