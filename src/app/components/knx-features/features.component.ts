5494aa9095a78fba61dfde9323746779dc874405
Matteo Salvestrini <matteo.s@hcl.com>
2017-05-29 16:10:53 +0200

    refactor(knx-features): remove classes conflicting with flexbox
    Remove row and the col-sm* classes since knx-features uses flexbox
    
    Signed-off-by: Matteo Salvestrini <matteo.s@hcl.com>

--- src/app/components/knx-features/features.component.ts
+++ src/app/components/knx-features/features.component.ts
@@ -5,8 +5,8 @@ import { Feature } from '../../models/feature';
   selector: 'knx-features',
   template: `
   <div class="container-fluid knx-container--fullwidth knx-container--gray">
-    <div class="knx-features row">
-      <div class="knx-features__item col-sm-{{ 12/items.length}}" *ngFor="let item of items">
+    <div class="knx-features">
+      <div class="knx-features__item" *ngFor="let item of items">
         <div class="knx-features__title">{{ item.title }}</div>
         <div class="knx-features__description">{{ item.description }}</div>
       </div>
