1a1a1bb89eb1c42c0c532a6c20bfb0f8964eaebe
Thomas Grendel <thomas-g@hcl.com>
2017-06-02 18:36:26 +0200

    feat(contact-detail): add load profile data and form submit

--- src/app/pages/car/buy/car-contact.component.ts
+++ src/app/pages/car/buy/car-contact.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+import { Profile } from './../../../models/profile';
 import { ContactDetailForm } from './../../../forms/contact-detail.form';
 
 @Component({
   selector: 'knx-car-contact-form',
   templateUrl: 'car-contact.component.html'
 })
-export class CarContactComponent implements OnInit {
+export class CarContactComponent implements OnChanges {
   @Input() form: ContactDetailForm;
+  @Input() profile: Profile;
 
-  constructor() {}
-
-  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.profile) {
+      this.form.formGroup.patchValue({
+        firstName: this.profile.firstname,
+        middleName: this.profile.infix,
+        lastName: this.profile.lastname,
+        mobileNumber: this.profile.phone
+      });
+    }
+   }
 }
