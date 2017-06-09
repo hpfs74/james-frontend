e467bc576f644894ec9d056f797363dc8425903b
Thomas Grendel <thomas-g@hcl.com>
2017-06-09 11:48:41 +0200

    test(car-contact): add more form tests

--- src/app/pages/car/buy/car-contact.component.spec.ts
+++ src/app/pages/car/buy/car-contact.component.spec.ts
@@ -6,15 +6,16 @@ import { BrowserModule, By } from '@angular/platform-browser';
 import { CXFormsModule } from '../../../../../node_modules/@cx/forms';
 import { CarContactComponent } from './car-contact.component';
 import { ContactDetailForm } from '../../../forms/contact-detail.form';
+import { nameInitialMask } from '../../../utils/base-form.utils';
 
 @Component({
-    template : `<div><knx-car-contact-form [form]="formFromHost" [profile]="profileFromHost"></knx-car-contact-form></div>`
+  template: `<div><knx-car-contact-form [form]="formFromHost" [profile]="profileFromHost"></knx-car-contact-form></div>`
 })
 export class TestHostComponent {
-    @ViewChild(CarContactComponent)
-    public carContactComponent: CarContactComponent;
-    public formFromHost: ContactDetailForm = new ContactDetailForm(new FormBuilder());
-    public profileFromHost: any;
+  @ViewChild(CarContactComponent)
+  public carContactComponent: CarContactComponent;
+  public formFromHost: ContactDetailForm = new ContactDetailForm(new FormBuilder());
+  public profileFromHost: any;
 }
 
 describe('Component: CarContactComponent', () => {
@@ -39,6 +40,39 @@ describe('Component: CarContactComponent', () => {
     expect(element).toBeDefined();
     expect(comp.carContactComponent).toBeDefined();
     expect(comp.carContactComponent.form).toBeDefined();
+  });
+
+  it('should have invalid form controls on init', () => {
+    expect(comp.carContactComponent.form.formGroup.valid).toBeFalsy();
+  });
+
+  it('should transform the initials without dots', () => {
+    expect(nameInitialMask.decode('A.B.')).toBe('AB');
+  });
+
+  it('should validate the mobile number', () => {
+    let ctrl = comp.carContactComponent.form.formGroup.get('mobileNumber');
+
+    ctrl.setValue('061234');
+    expect(comp.carContactComponent.form.formGroup.valid).toBeFalsy();
+    expect(ctrl.valid).toBeFalsy();
+
+    ctrl.setValue('0612345678');
+    expect(ctrl.valid).toBeTruthy();
+  });
+
+  it('should validate the phone number', () => {
+    let ctrl = comp.carContactComponent.form.formGroup.get('phoneNumber');
+    ctrl.setValue('061234');
+    expect(comp.carContactComponent.form.formGroup.valid).toBeFalsy();
+    expect(ctrl.valid).toBeFalsy();
+
+    // 09-06-2017 currently no distinction between mobile and landline number
+    ctrl.setValue('0612345678');
+    expect(ctrl.valid).toBeTruthy();
+
+    ctrl.setValue('0203031680');
+    expect(ctrl.valid).toBeTruthy();
   });
 
   it('should update the form with profile data (ngOnChanges)', () => {
