f037e772b807ce2d3a20acdadc51613d697af827
Alexey Trofimov <alexey.trofimov@hcl.com>
2017-06-28 18:30:17 +0200

    feat(styles): add css autoprefix

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -101,7 +101,7 @@ module.exports = function (options) {
          */
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
           include: [helpers.root('src', 'styles')]
         },
 
@@ -112,7 +112,7 @@ module.exports = function (options) {
          */
         {
           test: /\.scss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
+          use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
           include: [helpers.root('src', 'styles')]
         },
 
