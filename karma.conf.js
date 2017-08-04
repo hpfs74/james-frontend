74d553e555dd5b719d8b3aa545d99ea463c94332
Thomas Grendel <thomas-g@hcl.com>
2017-08-04 21:35:29 +0200

    fix(karma): fix karma with mocha and other reporters

--- karma.conf.js
+++ karma.conf.js
@@ -2,32 +2,84 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
+  var configuration = {
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
+      require('karma-mocha-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+
+      // require('karma-jasmine'),
+      // //require('karma-chrome-launcher'),
+      // require('karma-phantomjs-launcher'),
+      require('karma-junit-reporter'),
+      // require('karma-html-reporter'),
+      // require('karma-jasmine-html-reporter'),
+      // require('karma-coverage'),
+      // require('karma-coverage-istanbul-reporter'),
+      // require('karma-remap-istanbul'),
+      // require('karma-remap-coverage'),
+      // require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    /*
+     * test results reporter to use
+     * available reporters: https://npmjs.org/browse/keyword/karma-reporter
+     */
+    reporters: ['mocha', 'junit'],
+
+    coverageReporter: {
+      includeAllSources: true,
+      type: 'in-memory',
+      reporters: [
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'json', subdir: '.', file: 'coverage-final.json' },
+        { type: 'clover', dir: 'coverage', subdir: '.', file: 'clover.xml' }
+      ]
+    },
+
+    remapCoverageReporter: {
+      'text-summary': null,
+      json: './coverage/coverage.json',
+      html: './coverage/html'
+    },
+
+    junitReporter: {
+      outputDir: './coverage', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'jasmine.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: false, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {} // key value pair of properties to add to the <properties> section of the report
+    },
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
+    autoWatch: process.env.KARMA_WATCH ? true : false,
+    browsers: ['PhantomJS'],
+    singleRun: process.env.KARMA_WATCH ? false : true
+  };
+
+  // CI
+  if (!process.env.KARMA_WATCH) {
+    configuration.coverageReporter.reporters.push({ type: 'text' });
+    configuration.coverageReporter.reporters.push({ type: 'text-summary' });
+  }
+
+  config.set(configuration);
 };
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,20 +19,20 @@
  */
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
@@ -40,6 +40,7 @@
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+import 'core-js/client/shim';
 
 
 /**
@@ -55,6 +56,14 @@ import 'core-js/es7/reflect';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+/** These are needed by the spec build */
+// import 'zone.js/dist/long-stack-trace-zone';
+// import 'zone.js/dist/proxy'; // since zone.js 0.6.15
+// import 'zone.js/dist/sync-test';
+// import 'zone.js/dist/jasmine-patch'; // put here since zone.js 0.6.14
+// import 'zone.js/dist/async-test';
+// import 'zone.js/dist/fake-async-test';
+
 
 
 /***************************************************************************************************
@@ -65,7 +74,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-// import 'intl';  // Run `npm install --save intl`.
+import 'intl';  // Run `npm install --save intl`.
 /**
  * Need to import at least one locale-data with intl.
  */
