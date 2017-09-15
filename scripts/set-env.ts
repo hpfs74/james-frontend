09b45e4dd7c07f4d763c6e4e6517d87255b6da60
Matteo Salvestrini <matteo.s@hcl.com>
2017-09-15 09:45:00 +0000

    refactor(set-env): check for env vars to be present Bitbucket
    add some more check to throw error and break the build in case env var
    for configuration are missing

--- scripts/set-env.ts
+++ scripts/set-env.ts
@@ -14,9 +14,19 @@ const defaultTarget = './src/environments/environment.ts';
 const targetPath = isProd ? `./src/environments/environment.prod.ts` : defaultTarget;
 const logger = console.log;
 
+
+function getEnvVar(name) {
+  if (!process.env[name]) {
+    throw new Error(`Sorry cannot find any value for "${name}" env var. Please provide one!!!`);
+  }
+  return process.env[name];
+}
+
+
+
 // TODO: provide the correct client id
 const forgetPasswordLink =
-  `${process.env.NICCI_BASE_URL}` + '/password?client_id=56a6ab20bb00893f071faddc' +
+  `${getEnvVar('NICCI_BASE_URL')}` + '/password?client_id=56a6ab20bb00893f071faddc' +
   '&locale=nl_NL' +
   '&response_type=code' +
   '&scope=basic+emailaddress+social' +
@@ -26,41 +36,41 @@ let envConfigFile = `
 export const environment = {
   production: ${isProd},
   james: {
-    nicciKey: '${process.env.NICCI_KEY}',
+    nicciKey: '${getEnvVar('NICCI_KEY')}',
     forgetPassword: '${forgetPasswordLink}',
-    key: '${process.env.JAMES_API_KEY}',
-    token: '${process.env.JAMES_API_TOKEN}',
-    profile: '${process.env.JAMES_API_PROFILE}',
-    auth: '${process.env.JAMES_API_AUTH}',
-    address: '${process.env.JAMES_API_ADDRESS}',
-    cars: '${process.env.JAMES_API_CARS}',
-    carCompare: '${process.env.JAMES_API_CAR_COMPARE}',
-    carCoverage: '${process.env.JAMES_API_CAR_COVERAGE}',
-    carDamageFree: '${process.env.JAMES_API_CAR_DAMAGEFREE}',
-    carBuy: '${process.env.JAMES_API_CAR_BUY}',
-    insurer: '${process.env.JAMES_API_INSURER}',
+    key: '${getEnvVar('JAMES_API_KEY')}',
+    token: '${getEnvVar('JAMES_API_TOKEN')}',
+    profile: '${getEnvVar('JAMES_API_PROFILE')}',
+    auth: '${getEnvVar('JAMES_API_AUTH')}',
+    address: '${getEnvVar('JAMES_API_ADDRESS')}',
+    cars: '${getEnvVar('JAMES_API_CARS')}',
+    carCompare: '${getEnvVar('JAMES_API_CAR_COMPARE')}',
+    carCoverage: '${getEnvVar('JAMES_API_CAR_COVERAGE')}',
+    carDamageFree: '${getEnvVar('JAMES_API_CAR_DAMAGEFREE')}',
+    carBuy: '${getEnvVar('JAMES_API_CAR_BUY')}',
+    insurer: '${getEnvVar('JAMES_API_INSURER')}',
     payloadEncryption: {
       client: {
-        id: '${process.env.PAYLOAD_CLIENT_ID}',
-        secret: '${process.env.PAYLOAD_CLIENT_SECRET}',
+        id: '${getEnvVar('PAYLOAD_CLIENT_ID')}',
+        secret: '${getEnvVar('PAYLOAD_CLIENT_SECRET')}',
       },
-      key: '${process.env.PAYLOAD_KEY_URL}',
-      profile: '${process.env.PAYLOAD_PROFILE_URL}',
-      token: '${process.env.PAYLOAD_TOKEN_URL}',
-      login: '${process.env.PAYLOAD_LOGIN_URL}'
+      key: '${getEnvVar('PAYLOAD_KEY_URL')}',
+      profile: '${getEnvVar('PAYLOAD_PROFILE_URL')}',
+      token: '${getEnvVar('PAYLOAD_TOKEN_URL')}',
+      login: '${getEnvVar('PAYLOAD_LOGIN_URL')}'
     },
   }
 };
 `;
 
-if (isProd && process.env.GA_ID) {
+if (isProd && getEnvVar('GA_ID')) {
   envConfigFile += `document.write(\`
     <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script', 'https://www.google-analytics.com/analytics.js','ga');
-    ga('create', '${process.env.GA_ID}', 'auto');
+    ga('create', '${getEnvVar('GA_ID')}', 'auto');
     ga('send', 'pageview');
   </script>\`);`;
 }
