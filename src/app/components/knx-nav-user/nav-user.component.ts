9855a41f5b29e517e7c711376019e3f0b75aa908
Thomas Grendel <thomas-g@hcl.com>
2017-09-15 20:54:06 +0200

    fix(layout): improve navbar alignments

--- src/app/components/knx-nav-user/nav-user.component.ts
+++ src/app/components/knx-nav-user/nav-user.component.ts
@@ -12,7 +12,7 @@ import { Profile } from '../../models/profile';
         <knx-dropdown-button>
           <span class="knx-nav-user__icon knx-icon-user-o"></span> {{ profile?.firstname || 'Account' }}
         </knx-dropdown-button>
-        <knx-dropdown-menu offset="0 -10">
+        <knx-dropdown-menu offset="0 -55">
           <knx-menu-item *ngIf="showAccount" (click)="goToProfile()">
             <span>Mijn account</span>
           </knx-menu-item>
