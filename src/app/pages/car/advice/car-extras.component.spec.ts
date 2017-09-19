9036dbc1607192da6ff46f27662f44b032c575a8
Alexey Trofimov <alexey.trofimov@hcl.com>
2017-09-19 17:18:03 +0200

    test(extras): add tests for car extras

--- src/app/pages/car/advice/car-extras.component.spec.ts
+++ src/app/pages/car/advice/car-extras.component.spec.ts
@@ -1,20 +1,13 @@
 import { CarExtrasForm } from './car-extras.form';
-import { NO_ERRORS_SCHEMA, DebugElement, ViewChild, OnChanges, Input, Component } from '@angular/core';
+import { ViewChild, Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { BrowserModule, By } from '@angular/platform-browser';
 import { CXFormsModule } from '../../../../../node_modules/@cx/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from '../../../shared.module';
-import { CarInfoMessageComponent } from '../../../components/knx-car-info-message/car-info-message.component';
 import { CarExtrasComponent } from './car-extras.component';
-import { CarService } from '../car.service';
-import { AuthHttp } from '../../../services/auth-http.service';
-import { AuthService } from '../../../services/auth.service';
-import { LocalStorageService } from '../../../services/localstorage.service';
-import { LoaderService } from '../../../components/knx-app-loader/loader.service';
-import { CarReportingCodeForm } from '../buy/car-reporting-code.form';
 
 @Component({
   template: `<div><knx-car-extras [form]="formFromHost" [show]="true" optionModifierClass="hidden-xs"></knx-car-extras></div>`
@@ -22,15 +15,7 @@ import { CarReportingCodeForm } from '../buy/car-reporting-code.form';
 export class TestHostComponent {
   @ViewChild(CarExtrasComponent)
   public targetComponent: CarExtrasComponent;
-  public formFromHost: CarExtrasForm = new CarExtrasForm(
-    new FormBuilder(), [
-      {
-        value: 'SCM5',
-        baab: 'baba',
-        title: 'Test',
-        description: 'This is a description'
-      }
-    ]);
+  public formFromHost: CarExtrasForm = new CarExtrasForm(new FormBuilder());
 }
 
 describe('Component: CarExtrasComponent', () => {
@@ -40,8 +25,8 @@ describe('Component: CarExtrasComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [BrowserAnimationsModule, BrowserModule, FormsModule, ReactiveFormsModule, CXFormsModule, SharedModule],
-      providers: [ AuthHttp, AuthService, LocalStorageService, LoaderService, CarService ],
-      declarations: [CarExtrasComponent, CarInfoMessageComponent, TestHostComponent]
+      providers: [],
+      declarations: [CarExtrasComponent, TestHostComponent]
     }).compileComponents();
   }));
 
