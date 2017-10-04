bb38868713e409ffc34101801c8915ccc85414dc
Thomas Grendel <thomas-g@hcl.com>
2017-10-04 17:57:28 +0200

    test(metareducers): add logger and logout tests

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -99,31 +99,3 @@ export function logout(reducer: ActionReducer<State>): ActionReducer<State> {
 export const metaReducers: MetaReducer<State>[] = !environment.production
 ? [logger, logout] /* [logger, storeFreeze] */
 : [logout];
-
-/**
- * A selector function is a map function factory. We pass it parameters and it
- * returns a function that maps from the larger state tree into a smaller
- * piece of state. This selector simply selects the `books` state.
- *
- * Selectors are used with the `select` operator.
- *
- * ```ts
- * class MyComponent {
- * 	constructor(state$: Observable<State>) {
- * 	  this.booksState$ = state$.select(getBooksState);
- * 	}
- * }
- * ```
- */
-
-
-/**
- * Every reducer module exports selector functions, however child reducers
- * have no knowledge of the overall state tree. To make them useable, we
- * need to make new selectors that wrap them.
- *
- * The createSelector function from the reselect library creates
- * very efficient selectors that are memoized and only recompute when arguments change.
- * The created selectors can also be composed together to select different
- * pieces of state.
- */
