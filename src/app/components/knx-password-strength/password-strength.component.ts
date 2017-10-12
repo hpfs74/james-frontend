6f66d43afcd8623ba095609ac41a38fed36fabd3
Matteo Salvestrini <salve@conmet.it>
2017-10-12 13:47:57 +0200

    refactor(register): add password-strength component
    Signed-off-by: Matteo Salvestrini <salve@conmet.it>

---
+++ src/app/components/knx-password-strength/password-strength.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'knx-password-strength',
+  template: `
+    <div class="knx-password-strength">
+      <span>Minimaal</span>
+      <span ngClass="{ 'knx-password-error': passwordLength() }">8 karakters</span> -
+      <span ngClass="{ 'knx-password-error': containsAtLeastOneNumber() }">1 cijfer</span> -
+      <span ngClass="{ 'knx-password-error': containsAtLeastOneCap()">1 hoofletter</span> -
+      <span ngClass="{ 'knx-password-error': containerAtLeastOneLowerCase() }">1 kleine letter</span>
+    </div>
+  `
+})
+export class PasswordStrengthComponent implements OnInit {
+  @Input() password: string;
+
+  private dow = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  ngOnInit() {
+  }
+
+  passwordLength(): boolean {
+    return this.password.length >= 8;
+  }
+
+  containsAtLeastOneNumber(): boolean {
+    return /[0-9]/.test(this.password);
+  }
+
+  containsAtLeastOneCap(): boolean {
+    return /[A-Z]/.test(this.password);
+  }
+
+  containerAtLeastOneLowerCase(): boolean {
+    return /[a-z]/.test(this.password);
+  }
+}
