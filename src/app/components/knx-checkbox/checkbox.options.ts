b79dc09d80f3a3bce3a3194f0bdbc134356db8c7
Matteo Salvestrini <salve@conmet.it>
2017-10-13 23:00:10 +0200

    refactor(knx-checkbox): add inner content checkbox
    added a component to handle inner content of checkbox
    
    Signed-off-by: Matteo Salvestrini <salve@conmet.it>

---
+++ src/app/components/knx-checkbox/checkbox.options.ts
@@ -0,0 +1,24 @@
+import { FormControlOptions } from '@cx/form-control';
+
+export interface KNXCheckboxOptionsSingle extends FormControlOptions  {
+  label: string;
+}
+
+export interface KNXCheckboxOptionsMulti extends FormControlOptions  {
+  items: Array<{
+    label: string,
+    value: string
+  }>;
+}
+
+export interface KNXCheckboxOptions extends FormControlOptions {
+  items?: Array<{
+    label: string,
+    value: string
+  }>;
+  label?: string;
+}
+
+export const KNX_CHECKBOX_DEFAULT_OPTIONS: KNXCheckboxOptions = {
+  items: null,
+};
