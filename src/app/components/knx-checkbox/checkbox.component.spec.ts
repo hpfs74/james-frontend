9172c264bd881b87498a0ff7dc56a73c9f5fbc39
Matteo Salvestrini <salve@conmet.it>
2017-10-16 14:12:34 +0200

    refactor(knx-checkbox): add more unit test
    Signed-off-by: Matteo Salvestrini <salve@conmet.it>

--- src/app/components/knx-checkbox/checkbox.component.spec.ts
+++ src/app/components/knx-checkbox/checkbox.component.spec.ts
@@ -5,39 +5,98 @@ import { By } from '@angular/platform-browser';
 
 import { setUpTestBed } from './../../../test.common.spec';
 import { KNXCheckboxComponent } from './checkbox.component';
+import { KNXCheckboxOptions } from './checkbox.options';
+
 
 @Component({
   template: `
-    <knx-checkbox ></knx-checkbox>`
+    <knx-checkbox [options]="options"></knx-checkbox>`
 })
 export class TestHostComponent {
   @ViewChild(KNXCheckboxComponent)
   targetComponent: KNXCheckboxComponent;
+  public options: KNXCheckboxOptions = {
+    label: 'Test string'
+  };
 }
 
-describe('Component: CircleProgressComponent', () => {
-  let comp: TestHostComponent;
-  let fixture: ComponentFixture<TestHostComponent>;
-  let de: DebugElement;
-  let el: HTMLElement;
+@Component({
+  template: `
+    <knx-checkbox [options]="options"><i>Test</i></knx-checkbox>`
+})
+export class CheckBoxWithContentTestHostComponent {
+  @ViewChild(KNXCheckboxComponent)
+  targetComponent: KNXCheckboxComponent;
+  options: null;
+}
 
-  let moduleDef: TestModuleMetadata = {
-    imports: [BrowserAnimationsModule],
-    declarations: [TestHostComponent, KNXCheckboxComponent],
-    schemas: [NO_ERRORS_SCHEMA]
-  };
-  setUpTestBed(moduleDef);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestHostComponent);
-    comp = fixture.componentInstance;
-    fixture.detectChanges();
-    de = fixture.debugElement.query(By.css('div.knx-collapse-message'));
-    el = de.nativeElement;
+describe('Component: KNXCheckBoxComponent', () => {
+
+  describe('Test without inner content', () => {
+    let comp: TestHostComponent;
+    let fixture: ComponentFixture<TestHostComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    let moduleDef: TestModuleMetadata = {
+      imports: [BrowserAnimationsModule],
+      declarations: [TestHostComponent, KNXCheckboxComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    };
+
+    setUpTestBed(moduleDef);
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TestHostComponent);
+      comp = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should be initialized', () => {
+      expect(comp).toBeDefined();
+    });
+
+    it('should display label content', () => {
+      fixture.detectChanges();
+      de = fixture.debugElement.query(By.css('span.cx-checkbox__label'));
+      el = de.nativeElement;
+
+      expect(el.innerHTML).toContain('Test string');
+    });
   });
 
-  xit('should be initialized');
-  xit('should display inner content if no lables');
-  xit('should display lables if present');
+  describe('Test with inner content', () => {
+    let comp: CheckBoxWithContentTestHostComponent;
+    let fixture: ComponentFixture<CheckBoxWithContentTestHostComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    let moduleDef: TestModuleMetadata = {
+      imports: [BrowserAnimationsModule],
+      declarations: [CheckBoxWithContentTestHostComponent, KNXCheckboxComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    };
+
+    setUpTestBed(moduleDef);
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(CheckBoxWithContentTestHostComponent);
+      comp = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should be initialized', () => {
+      expect(comp).toBeDefined();
+    });
+
+    it('should display label content', () => {
+      fixture.detectChanges();
+      de = fixture.debugElement.query(By.css('span.cx-checkbox__label'));
+      el = de.nativeElement;
+
+      expect(el.innerHTML).toContain('Test');
+    });
+  });
 });
 
