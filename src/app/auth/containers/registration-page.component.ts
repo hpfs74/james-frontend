0d2c7d9e3359c197a4d236e5a977b544a5a119f7
mitopalov <mitopalov@gmail.com>
2017-10-20 11:57:42 +0200

    style(registration-page): whitespace added

--- src/app/auth/containers/registration-page.component.ts
+++ src/app/auth/containers/registration-page.component.ts
@@ -8,12 +8,14 @@ import { CXEmailValidator } from '@cx/form';
 @Component({
   selector: 'knx-password-reset',
   template: `
-      <div class="row">
-        <div class="col-4 offset-2 flex-start">
+      <div class="container">
+        <div class="row">
+        <div class="col-md-6 p-3">
           <knx-registration></knx-registration>
         </div>
-        <div class="col-4 flex-end">
+        <div class="col-md-6 p-3">
           <knx-download-panel></knx-download-panel>
+        </div>
         </div>
       </div>
   `
