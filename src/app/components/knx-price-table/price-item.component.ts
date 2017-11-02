54106c0a916d48520029a364590f9f712d267d0d
rokalica <mitopalov@gmail.com>
2017-11-02 09:25:08 +0100

    style(knx-list): changed list indentation on price-item component

--- src/app/components/knx-price-table/price-item.component.ts
+++ src/app/components/knx-price-table/price-item.component.ts
@@ -2,20 +2,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'knx-price-item',
-  template: `
-  <div class="knx-price-item" [ngClass]="{'knx-price-item--highlight': highlight, 'knx-price-item--selected': selected }">
-    <div class="knx-price-item__header" >
-      {{ header }}
-      <div *ngIf="highlight" class="knx-price-item__badge">{{ badge }}</div>
-    </div>
-    <ul class="knx-price-item__features">
-      <li *ngFor="let item of features">{{ item }}</li>
-    </ul>
-    <div *ngIf="price" class="knx-price-item__price">
-      vanaf <span>{{ price | currency:'EUR':true:'1.2-2' }}</span>
-    </div>
-    <button class="knx-button knx-button--ghost">{{ selected ? ' Gekozen' : 'Kies deze' }}</button>
-  </div>`
+  templateUrl: './price-item.component.html',
+  styleUrls: ['./price-item.scss']
 })
 export class PriceItemComponent {
   @Input() header: string;
