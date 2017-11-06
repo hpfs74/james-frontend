fb6f71cde6051c8781ecfb1f0a00766875601864
Matteo Salvestrini <salve@conmet.it>
2017-11-06 13:05:58 +0100

    refactor(service-guide): add visibility on insurance loaded
    add visible attribute to show only when insurances are loaded
    add link to external pdf
    
    Signed-off-by: Matteo Salvestrini <salve@conmet.it>

--- src/app/components/knx-service-guide/service-guide.ts
+++ src/app/components/knx-service-guide/service-guide.ts
@@ -1,21 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'knx-service-guide',
   styleUrls: ['./service-guide.scss'],
   template: `
-    <div class="container knx-service-guide">
+    <div class="container knx-service-guide" *ngIf="visible">
       <div class="row">
         <div class="col">
           Knab Verzekeren is een handelsnaam van Knab Advies en Bemiddeling N.V., een onderdeel van Aegon.
           Knab Verzekeren is geregistreerd bij de AFM en ingeschreven bij de KvK.
-          Meer informatie vind je in de <a target="_blank" href="/assets/docs/knab-verzekeren-dienstenwijzer.pdf">Knab
-          Verzekeren Dienstenwijzer (PDF)</a>.
+          Meer informatie vind je in de
+          <a target="_blank" href="https://www.knab.nl/~/media/files/overeenkomsten-en-voorwaarden/dienstenwijzer.pdf">Knab
+            Verzekeren Dienstenwijzer (PDF)</a>.
         </div>
       </div>
     </div>
   `
 })
 export class ServiceGuideComponent {
+  @Input() visible = false;
 
 }
