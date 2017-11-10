b23486330fdbe1329f73fdaef509ff7170256e35
Thomas Grendel <thomas-g@hcl.com>
2017-11-10 10:12:08 +0100

    chore(slider-util): remove unused slider util

--- src/app/utils/slider.util.spec.ts
+++
@@ -1,8 +0,0 @@
-import { getRangeConfig } from './slider.util';
-
-describe('Slider Util', () => {
-  it('should generate a range of values', () => {
-    const ranges = [0, 10, 20, 30, 40];
-    expect(getRangeConfig(ranges)).toBeDefined();
-  });
-});
