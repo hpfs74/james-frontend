14491bad06293e064558c3e7456ed8a147e276cc
Alexey Trofimov <alexey.trofimov@hcl.com>
2017-11-30 15:18:26 +0100

    feat(design): introduce new advices cards
    Signed-off-by: Alexey Trofimov <alexey.trofimov@hcl.com>

--- src/app/components/knx-insurance-result/insurance-result.component.ts
+++ src/app/components/knx-insurance-result/insurance-result.component.ts
@@ -11,59 +11,64 @@ import { InsuranceAdvice, Insurer } from '../../insurance/models';
       <div class="knx-insurance_advice knx-card" [ngClass]="{'knx-insurance_advice__supported': insurance.supported}">
         <div class="knx-insurance-result__counter">{{ index + 1 }}</div>
         <div class="row">
-          <div class="col-sm-3">
-            <img class="knx-insurance-result__logo" src="{{ insurance._embedded.insurance.insurance_logo }}">
+          <div class="knx-insurance-result__bookmark" *ngIf="insurance.supported">knab</div>
+          <div class="knx-insurance-result__bookmark-tip" *ngIf="insurance.supported"></div>
+
+          <div class="col-md-3">
+            <img class="knx-insurance-result__logo" src="{{ insurance._embedded.insurance.insurance_logo }}" alt="logo">
           </div>
 
-          <div class="col-sm-5">
+          <div class="col-md-5 knx-insurance-result__data">
             <div class="row">
-              <div class="col-sm-6">
+              <div class="col-md-6 col-6">
                 <div class="knx-insurance-result__value">{{insurance.fit}}%</div>
                 <div class="knx-insurance-result__label">
-                  Profielscore<span class="knx-info-icon knx-icon-info-circle" style="cursor: pointer;">
-                    <knx-tooltip>De profielscore geeft aan hoe goed de verzekering bij jouw profiel past. Hoe hoger de score, hoe beter.
-                    Een score van minstens 75% betekent dat de verzekering goed aansluit bij jouw wensen en situatie.</knx-tooltip>
+                  <span class="knx-insurance-result__label--no-break">Profielscore
+                    <span class="knx-info-icon knx-icon-info-circle" style="cursor: pointer;">
+                      <knx-tooltip>De profielscore geeft aan hoe goed de verzekering bij jouw profiel past. Hoe hoger de score, hoe beter.
+                      Een score van minstens 75% betekent dat de verzekering goed aansluit bij jouw wensen en situatie.</knx-tooltip>
+                    </span>
                   </span>
                 </div>
               </div>
 
-              <!--div class="col-sm-6 knx-insurance-result__reviews">
-                9.3<br><span>{{ insurance.reviews_amount }} reviews</span>
-              </div-->
+              <div class="col-md-6 col-6 knx-insurance-result__price-quality">
+                <div class="knx-insurance-result__value">
+                  {{ insurance.price_quality }}<span class="knx-insurance-result__value__sup">/10</span>
+                </div>
 
-              <div class="col-sm-6 knx-insurance-result__price-quality">
-                <div class="knx-insurance-result__value">{{ insurance.price_quality }}<span>/10</span></div>
                 <div class="knx-insurance-result__label">
-                  Prijs kwaliteit<span class="knx-info-icon knx-icon-info-circle" style="cursor: pointer;">
-                    <knx-tooltip>We berekenen de score op basis van het profiel dat jij instelt. We toetsen de verzekering op meer
-                    dan 100 criteria om te bepalen of wat jij belangrijk vindt, ook goed scoort binnen de verzekering.
-                    Zo betaal je voor wat jij belangrijk vindt en voorkom je verrassingen.</knx-tooltip>
+                  Prijs
+                  <span class="knx-insurance-result__label--no-break">
+                    kwaliteit
+                    <span class="knx-info-icon knx-icon-info-circle" style="cursor: pointer;">
+                      <knx-tooltip>We berekenen de score op basis van het profiel dat jij instelt. We toetsen de verzekering op meer
+                      dan 100 criteria om te bepalen of wat jij belangrijk vindt, ook goed scoort binnen de verzekering.
+                      Zo betaal je voor wat jij belangrijk vindt en voorkom je verrassingen.</knx-tooltip>
+                    </span>
                   </span>
                 </div>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-sm-6 knx-insurance-result__value">
+              <div class="col-md-6 col-6 knx-insurance-result__value">
                 <div class="knx-insurance-result__value">{{ insurance.own_risk | currency:'EUR':true }}</div>
                 <div class="knx-insurance-result__label">Maximaal eigen risico</div>
               </div>
 
-              <div class="col-sm-6 knx-insurance-result__value">
+              <div class="col-md-6 col-6 knx-insurance-result__value">
                 <div class="knx-insurance-result__value">{{ insurance.main_coverage | jamesTag: 'car_flow_coverage' }}</div>
                 <div class="knx-insurance-result__label">Dekking</div>
               </div>
 
-              <!--<div class="col-sm-6 knx-insurance-result__profilescore">-->
+              <!--<div class="col-md-6 knx-insurance-result__profilescore">-->
                 <!--<knx-donut *ngIf="insurance.fit" [percentage]="insurance.fit"></knx-donut>-->
               <!--</div>-->
             </div>
           </div>
 
-          <div class="col-sm-4 knx-insurance-result__buy">
-            <div class="knx-insurance-result__bookmark" *ngIf="insurance.supported">knab</div>
-            <div class="knx-insurance-result__bookmark-tip" *ngIf="insurance.supported"></div>
-
+          <div class="col-md-4 knx-insurance-result__buy">
             <div class="knx-insurance-result__premium knx-insurance-result__price">
               {{ insurance.monthly_premium | currency:'EUR':true }} <span>per maand</span>
             </div>
@@ -80,6 +85,7 @@ import { InsuranceAdvice, Insurer } from '../../insurance/models';
       </div>
 
       <div class="knx-insurance-result__discount" *ngIf="insurance.discount">
+        <img src="/assets/icon/present_icon.png" alt="present">
         Inc. {{ insurance.discount | currency:'EUR':true }} Knab korting & Gratis overstapservice
       </div>
       <!--<knx-insurance-result-detail *ngIf="showDetailPanel && insurance.insurer" [insurer]="insurer"></knx-insurance-result-detail>-->
