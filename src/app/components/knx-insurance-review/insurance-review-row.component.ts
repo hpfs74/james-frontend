ed7af0627c4782162677534c21acdbad6d0ee6c8
Mirnes Topalovic <mitopalov@gmail.com>
2017-12-13 16:53:29 +0100

    styles(insurance-review-row): update styles for mobile

--- src/app/components/knx-insurance-review/insurance-review-row.component.ts
+++ src/app/components/knx-insurance-review/insurance-review-row.component.ts
@@ -32,22 +32,25 @@ export class InsuranceReviewRowContentComponent {
   selector: 'knx-ir-row',
   styleUrls: ['./insurance-review-row.component.scss'],
   template: `
-    <hr class="knx-ir-row__divider" *ngIf="newSection">
+    <hr [class.knx-ir-row-margin]="showIcon" class="knx-ir-row__divider" *ngIf="newSection">
     <div class="row">
-      <div class="col" [ngClass]="{
-        'col-md-5': showValue && !showIcon,
-        'col-md-12': !showValue && !showIcon,
-        'col-md-8 knx-ir-icon': showIcon
-      }">
-        <ng-content select="knx-ir-icon"></ng-content>
-        <ng-content select="knx-ir-label"></ng-content>
-        <ng-content *ngIf="showTooltip" select="knx-ir-tooltip"></ng-content>
+      <div [ngClass]="{'col-md-1 col-2': showIcon, 'hide': !showIcon}">
+        <div class="knx-ir-icon">
+          <ng-content select="knx-ir-icon"></ng-content>
+        </div>
       </div>
-      <div *ngIf="showValue" [ngClass]="{
-        'col-md-5': showValue && !showIcon,
-        'col-md-4 knx-ir-icon': showIcon
-      }">
-        <ng-content select="knx-ir-value"></ng-content>
+      <div [ngClass]="{'col-md-12 col-12': !showIcon, 'col-md-11 col-10 knx-ir-icon': showIcon}">
+        <div class="container">
+          <div class="row">
+            <div [ngClass]="{'col-md-12 col-12': !showValue, 'col-md-6': showValue}">
+              <ng-content select="knx-ir-label"></ng-content>
+              <ng-content *ngIf="showTooltip" select="knx-ir-tooltip"></ng-content>
+            </div>
+            <div class="col-md-6">
+              <ng-content select="knx-ir-value"></ng-content>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   `
