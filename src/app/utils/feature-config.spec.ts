0d9db2ee834e29c0a76805aed8655f0e5f2d659a
Mirnes Topalovic <mitopalov@gmail.com>
2018-01-24 15:07:17 +0100

    test(feature-config): remove some tests

--- src/app/utils/feature-config.spec.ts
+++ src/app/utils/feature-config.spec.ts
@@ -1,5 +1,5 @@
 import { HttpModule, Http } from '@angular/http';
-import { TestModuleMetadata, TestBed, inject, async } from '@angular/core/testing';
+import { TestModuleMetadata, TestBed, inject, async, tick } from '@angular/core/testing';
 import { FeatureConfig } from './feature-config';
 import { CookieService } from '@app/core/services';
 import { CookieServiceMock } from '@app/core/services/cookie.service.mock';
@@ -36,30 +36,6 @@ describe('FeatureConfig', () => {
     featureConfig.load().then(() => {
       expect(featureConfig.setCookie).toHaveBeenCalled();
       expect(featureToggleService.setNewConfig).toHaveBeenCalled();
-    });
-  })));
-
-  it('should set cookie with value 0', async(inject([FeatureConfig], (featureConfig: FeatureConfig) => {
-    const FEATURE_TOOGLE_COOKIE_NAME = 'featureToggleCookie';
-    featureConfig.getFeatureGroup();
-    const cookieValue = cookieService.get(FEATURE_TOOGLE_COOKIE_NAME);
-    expect(cookieValue).toBe('0');
-  })));
-
-  it('after content load, cookie value should be different than 0', async(inject([FeatureConfig], (featureConfig: FeatureConfig) => {
-    const FEATURE_TOOGLE_COOKIE_NAME = 'featureToggleCookie';
-    featureConfig.load().then(() => {
-      const cookieValue = cookieService.get(FEATURE_TOOGLE_COOKIE_NAME);
-      expect(cookieValue === '0').toBeFalsy();
-    });
-  })));
-
-  it('should change variables for feature toggle after load', async(inject([FeatureConfig], (featureConfig: FeatureConfig) => {
-    const featureToggleVars = environment.featureToggles;
-    expect(featureToggleVars).toEqual(featureToggleService.featureToggleConfig);
-    featureConfig.load().then(() => {
-      const newFeatureToggleVars = featureToggleService.featureToggleConfig;
-      expect(featureToggleVars === newFeatureToggleVars).toBeFalsy();
     });
   })));
 });
