6b7253bd48b6fb62ffe6389f5821156198f05ba5
Mirnes Topalovic <mitopalov@gmail.com>
2018-01-26 16:41:02 +0100

    feat(car-contact): change reset button behaviour

--- src/app/car/submodules/car-buy/containers/car-contact/car-contact.component.ts
+++ src/app/car/submodules/car-buy/containers/car-contact/car-contact.component.ts
@@ -79,10 +79,8 @@ export class CarContactComponent implements QaIdentifier, AfterContentInit, OnDe
     this.store$.dispatch(new assistant.AddCannedMessage({key: 'car.buy.fill'}));
   }
 
-  resetFlow() {
-    this.store$.dispatch(new advice.RemoveLatestInsuranceAdvice());
-    this.store$.dispatch(new auth.ResetStates());
-    this.store$.dispatch(new router.Go({path: ['car']}));
+  goBack() {
+    this.store$.dispatch(new router.Back());
   }
 
   goToPreviousStep() {
