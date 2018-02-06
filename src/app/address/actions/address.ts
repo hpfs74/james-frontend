9013a0e24a87cc433ee3f24bb522077b289df0bc
Mirnes Topalovic <mitopalov@gmail.com>
2018-02-06 16:12:33 +0100

    fix(address): add option to clear address data from store

--- src/app/address/actions/address.ts
+++ src/app/address/actions/address.ts
@@ -4,6 +4,7 @@ import { Address, AddressLookup } from '../models';
 export const GET_ADDRESS_REQUEST = '[Address] Address Lookup';
 export const GET_ADDRESS_SUCCESS = '[Address] Address Lookup Success';
 export const GET_ADDRESS_FAILURE = '[Address] Address Lookup Failure';
+export const CLEAR_ADDRESS = '[Address] Clear Address';
 
 export class GetAddress implements Action {
   readonly type = GET_ADDRESS_REQUEST;
@@ -23,8 +24,13 @@ export class GetAddressFailure implements Action {
   constructor(public payload: any) {}
 }
 
+export class ClearAddress implements Action {
+  readonly type = CLEAR_ADDRESS;
+}
+
 
 export type All
 = GetAddress
 | GetAddressSuccess
+| ClearAddress
 | GetAddressFailure;
