e432f02ffc3cddd94896a2989f30c430297bee42
Matteo <matteo.s@hcl.com>
2018-02-10 11:52:05 +0100

    refactor(house-hold-service): refactor with httpclient
    Signed-off-by: Matteo <matteo.s@hcl.com>
    Signed-off-by: Matteo <matteo.s@hcl.com>

--- src/app/house/services/house-hold.service.ts
+++ src/app/house/services/house-hold.service.ts
@@ -1,41 +1,63 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response, URLSearchParams, RequestOptions, ResponseOptions } from '@angular/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { environment } from '@env/environment';
 import { HouseHoldAmountRequest, HouseHoldAmountResponse } from '@app/house/models/house-hold-amount';
 import { HouseDataResponse, HouseDataRequest } from '@app/house/models/house-data';
 import { HouseHoldPremiumRequest, HouseHoldPremiumResponse } from '@app/house/models/house-hold-premium';
-import { Observable } from 'rxjs/Observable';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable()
+/**
+ * handle all the request to the house hold service
+ */
 export class HouseHoldService {
-  constructor(private http: Http) {
+  constructor(private http: HttpClient) {
   }
 
-  public getHouseData(req: HouseDataRequest) {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    const url = `${environment.riskInsurance.getHouseData}?`
-      + `Zipcode=${req.Zipcode}&houseNumber=${req.HouseNumber}&houseNumberAddition=${req.HouseNumberAddition || ''}`;
-    return this.http.get(url, {headers: headers})
-      .map(res => <HouseDataResponse>res.json());
+  /**
+   * get information about the house
+   * @param {HouseDataRequest} req - address of the house
+   * @returns {Observable<HouseDataResponse>}
+   */
+  public getHouseData(req: HouseDataRequest): Observable<HouseDataResponse> {
+    const params = new HttpParams();
+
+    params.append('Zipcode', req.Zipcode);
+    params.append('houseNumber', req.HouseNumber.toString());
+    params.append('houseNumberAddition', req.HouseNumberAddition || '');
+
+    return this.http.get<HouseDataResponse>(environment.riskInsurance.getHouseData,
+      Object.assign(httpOptions, {params: params}));
   }
 
+  /**
+   * get the amount of supported insurance
+   * @param {HouseHoldAmountRequest} req
+   * @returns {Observable<HouseHoldAmountResponse>}
+   */
   public calculateHouseHoldAmount(req: HouseHoldAmountRequest): Observable<HouseHoldAmountResponse> {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
 
-    return this.http.post(environment.riskInsurance.HouseHoldAmount, req, {headers: headers})
-      .map(res => <HouseHoldAmountResponse>res.json())
+    return this.http.post<HouseHoldAmountResponse>(environment.riskInsurance.HouseHoldAmount, req, {headers: httpOptions.headers})
       .map(res => Object.assign(res, {InsuredAmount: res.InsuredAmount / 100.0}));
   }
 
+  /**
+   * get all insurance
+   * @param {HouseHoldPremiumRequest} req
+   * @returns {Observable<HouseHoldPremiumResponse>}
+   */
   public calculatePremiums(req: HouseHoldPremiumRequest): Observable<HouseHoldPremiumResponse> {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
 
-    return this.http.post(environment.riskInsurance.HouseHoldPremium, req, {headers: headers})
-      .map(res => <HouseHoldPremiumResponse>res.json());
+    return this.http.post<HouseHoldPremiumResponse>(environment.riskInsurance.HouseHoldPremium, req, {headers: httpOptions.headers})
+      .map(res => <HouseHoldPremiumResponse>res);
   }
 }
