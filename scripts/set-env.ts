d1521f3a50fd769a00f781e38c1295127aa36b9f
Mirnes Topalovic <mitopalov@gmail.com>
2018-02-14 14:55:20 +0100

    fix(feature-service): move script init into feature service

--- scripts/set-env.ts
+++ scripts/set-env.ts
@@ -22,23 +22,6 @@ function getEnvVar(name) {
   return process.env[name];
 }
 
-/* tslint:disable */
-function setAbTestingScript() {
-  return `
-    let abTestScript = document.createElement('script');
-    abTestScript.innerHTML =
-      \`var _vwo_code=(function(){
-        var account_id=${getEnvVar('VWO_ID')},
-        settings_tolerance=2000,
-        library_tolerance=2500,
-        use_existing_jquery=false,
-        /* DO NOT EDIT BELOW THIS LINE */
-        f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();\`
-
-    document.head.insertBefore(abTestScript, document.head.firstChild);
-  `;
-}
-
 function getGoogleAnalytics() {
   return `document.write(\`
     <script>
@@ -133,6 +116,7 @@ function getContent(environment: string) {
     domain: '${getEnvVar('JAMES_DOMAIN')}',
     production: ${isProd},
     enableAnalytics: ${enableAnalytics},
+    vwoId: ${getEnvVar('VWO_ID')},
     featureToggles: {
       enableBuyFlowEmail: ${getEnvVar('FEATURE_BUY_FLOW_EMAIL')},
       enableAnalyticsLogging: ${getEnvVar('FEATURE_ANALYTICS_LOGGING')},
@@ -196,7 +180,7 @@ function getContent(environment: string) {
     content += outputGtmSnippet(gtmAuth, gtmId, gtmVersion, environment);
     content += outputGtmNoScript(gtmAuth, gtmId, gtmVersion, environment);
   }
-  content += setAbTestingScript();
+
   content = '/* tslint:disable */' + content;
   content += `/* tslint:enable */\n`;
 
