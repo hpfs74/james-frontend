df60931c15534296e8082520164a83801d45200f
Mirnes Topalovic <mitopalov@gmail.com>
2018-02-14 11:23:16 +0100

    feat(set-env): add vwo script

--- scripts/set-env.ts
+++ scripts/set-env.ts
@@ -1,4 +1,4 @@
-import { writeFile, existsSync } from 'fs';
+import { writeFile, existsSync, readFile } from 'fs';
 import { argv } from 'yargs';
 
 require('dotenv').config();
@@ -10,6 +10,7 @@ const enableAnalytics = argv.analytics || false;
 const isWatchMode = argv['no-regenerate'] || false;
 const prodEnvFilePath = './src/environments/environment.prod.ts';
 const devEnvFilePath = './src/environments/environment.ts';
+const mainIndexPath = './src/index.html';
 
 function isProduction(environment: string) {
   return environment === 'production' || environment === 'prod';
@@ -20,6 +21,23 @@ function getEnvVar(name) {
     throw new Error(`Sorry cannot find any value for "${name}" env var. Please provide one!!!`);
   }
   return process.env[name];
+}
+
+/* tslint:disable */
+function setAbTestingScript() {
+  return `
+    let abTestScript = document.createElement('script');
+    abTestScript.innerHTML =
+      \`var _vwo_code=(function(){
+        var account_id=${getEnvVar('VWO_ID')},
+        settings_tolerance=2000,
+        library_tolerance=2500,
+        use_existing_jquery=false,
+        /* DO NOT EDIT BELOW THIS LINE */
+        f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();\`
+
+    document.head.insertBefore(abTestScript, document.head.firstChild);
+  `;
 }
 
 function getGoogleAnalytics() {
@@ -178,7 +196,7 @@ function getContent(environment: string) {
     content += outputGtmSnippet(gtmAuth, gtmId, gtmVersion, environment);
     content += outputGtmNoScript(gtmAuth, gtmId, gtmVersion, environment);
   }
-
+  content += setAbTestingScript();
   content = '/* tslint:disable */' + content;
   content += `/* tslint:enable */\n`;
 
