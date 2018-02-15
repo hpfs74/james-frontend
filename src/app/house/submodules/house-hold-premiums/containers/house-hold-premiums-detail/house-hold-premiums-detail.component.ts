ae034391f7e4580f2a0d385bdfc712b3509a9477
Matteo <matteo.s@hcl.com>
2018-02-15 08:14:38 +0100

    feat(house-hold): completed buy flow
    Signed-off-by: Matteo <matteo.s@hcl.com> 
    Signed-off-by: Matteo <matteo.s@hcl.com>

--- src/app/house/submodules/house-hold-premiums/containers/house-hold-premiums-detail/house-hold-premiums-detail.component.ts
+++ src/app/house/submodules/house-hold-premiums/containers/house-hold-premiums-detail/house-hold-premiums-detail.component.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs/Observable';
 
 import * as fromHouseHold from '@app/house/reducers';
 import { Subscription } from 'rxjs/Subscription';
+import { KNXStepError, KNXWizardStepRxOptions } from '@app/components/knx-wizard-rx/knx-wizard-rx.options';
+import * as router from '@core/actions/router';
 
 @Component({
   selector: 'knx-house-hold-premiums-detail',
@@ -14,12 +16,21 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./house-hold-premiums-detail.component.scss']
 })
 export class HouseHoldPremiumsDetailComponent implements OnInit, OnDestroy {
+  currentStepOptions: KNXWizardStepRxOptions;
+  error$: Observable<KNXStepError>;
 
   selectedInsurance$: Observable<CalculatedPremium>;
   insurance: CalculatedPremium;
   subscriptions: Subscription[] = [];
 
   constructor(private store$: Store<fromRoot.State>) {
+    this.currentStepOptions = {
+      backButtonLabel: 'Terug',
+      nextButtonLabel: 'Vraag direct aan',
+      hideBackButton: false,
+      hideNextButton: false,
+      nextButtonClass: 'knx-button knx-button--3d knx-button--primary'
+    };
   }
 
   ngOnInit() {
@@ -38,6 +49,10 @@ export class HouseHoldPremiumsDetailComponent implements OnInit, OnDestroy {
    */
   ngOnDestroy() {
     this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  goToNextStep() {
+    this.store$.dispatch(new router.Go({path: ['/household/premiums/buy']}));
   }
 }
 
--- src/app/house/submodules/house-hold-premiums/containers/house-hold-premiums/house-hold-premiums.component.ts
+++ src/app/house/submodules/house-hold-premiums/containers/house-hold-premiums/house-hold-premiums.component.ts
@@ -172,27 +172,29 @@ export class HouseHoldPremiumsComponent implements OnInit, OnDestroy, QaIdentifi
     }));
   }
 
+  /**
+   * go back to the advice last page that is dekking
+   */
   goBack() {
     this.store$.dispatch(new router.Go({
       path: ['/household/dekking']
     }));
   }
 
+  /**
+   * handle the button click on top of the page to go back to result page
+   */
   goToList() {
     this.store$.dispatch(new router.Go({
       path: ['/household/premiums/list']
     }));
   }
 
-  goToDetail() {
-    this.store$.dispatch(new router.Go({path: ['/household/premiums/detail']}));
-  }
-
+  /**
+   * handle the button click for the detail page
+   */
   goToBuy() {
     this.store$.dispatch(new router.Go({path: ['/household/premiums/buy']}));
   }
 
-  goToThankYou() {
-    this.store$.dispatch(new router.Go({path: ['/household/premiums/thankyou']}));
-  }
 }
