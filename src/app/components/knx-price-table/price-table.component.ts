fb0553c31f74b98a46b9e6c49e3d5c6be21f8256
Matteo <matteo.s@hcl.com>
2018-02-28 19:23:28 +0100

    chore(price-table): move html to template file
    Signed-off-by: Matteo <matteo.s@hcl.com>

--- src/app/components/knx-price-table/price-table.component.ts
+++ src/app/components/knx-price-table/price-table.component.ts
@@ -9,43 +9,7 @@ interface PriceTableLabelOptions {
 @Component({
   selector: 'knx-price-table',
   styleUrls: ['./price-table.component.scss'],
-  template: `
-    <div class="knx-price-table">
-      <div class="knx-recommended-wrapper visible-xs">
-        <div *ngFor="let item of items; let i = index"
-             [hidden]="!item.highlight"
-             class="knx-recommended">
-          {{ item.badge }}
-          <div class="knx-recommended__tick knx-recommended__tick--{{ i }}"></div>
-        </div>
-      </div>
-
-      <div class="knx-button knx-button--toggle-pills visible-xs"
-           *ngFor="let item of items; let i = index"
-           (click)="selectItem(i, true)"
-           [attr.data-btn]="item.selected ? item.dataActive : item.dataInactive"
-           [ngClass]="{ '': !item.selected, 'knx-button--toggle-pills-active': item.selected }">
-        {{ item.header }}
-      </div>
-
-      <knx-price-item
-        [ngClass]="{ 'knx-price-item--highlight': item.highlight }"
-        *ngFor="let item of items; let i = index"
-        [id]="item.id"
-        [header]="item.header"
-        [badge]="item.badge"
-        [price]="item.price"
-        [highlight]="item.highlight"
-        [selected]="item.selected"
-        [features]="item.features"
-        [dataActive]="item.dataActive"
-        [dataInactive]="item.dataInactive"
-        [selectedLabel]="labels.selected"
-        [unselectedLabel]="labels.unselected"
-        [description]="item.description"
-        (click)="selectItem(i, false)">
-      </knx-price-item>
-    </div>`
+  templateUrl: './price-table.component.html'
 })
 export class PriceTableComponent implements OnDestroy {
   @Output() onSelected: EventEmitter<Price> = new EventEmitter();
