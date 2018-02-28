1211a42539b56da7a99ad54dd1d508e43d404c7b
Mirnes Topalovic <mitopalov@gmail.com>
2018-02-28 13:09:53 +0100

    wip

--- src/app/components/knx-nav-user/nav-user.component.ts
+++ src/app/components/knx-nav-user/nav-user.component.ts
@@ -8,27 +8,7 @@ import { Store } from '@ngrx/store';
 @Component({
   selector: 'knx-nav-user',
   styleUrls: ['./nav-user.component.scss'],
-  template: `
-    <div class="knx-nav-user" *ngIf="profile" >
-      <knx-dropdown>
-        <knx-dropdown-button>
-          <span class="knx-nav-user__icon knx-icon-user-o"></span> {{ profile?.firstname || 'Account' }}
-        </knx-dropdown-button>
-
-        <knx-dropdown-menu offset="0 -55">
-          <knx-menu-item *ngIf="showAccount" (click)="goToProfile()">
-            <span>Mijn account</span>
-          </knx-menu-item>
-
-          <div class='knx-menu-divider'></div>
-
-          <knx-menu-item (click)="logOut()">
-            <span class="knx-icon-sign-out"></span> Uitloggen
-          </knx-menu-item>
-        </knx-dropdown-menu>
-      </knx-dropdown>
-    </div>
-  `
+  templateUrl: './nav-user.component.html'
 })
 export class NavUserComponent {
   @Input() profile: Profile;
