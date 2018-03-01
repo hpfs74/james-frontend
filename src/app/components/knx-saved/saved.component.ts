a95d1f01d52f0a22b1aa484835af0cf1e07ee669
Mirnes Topalovic <mitopalov@gmail.com>
2018-03-01 21:16:09 +0100

    feat(knx-saved): add pending class

--- src/app/components/knx-saved/saved.component.ts
+++ src/app/components/knx-saved/saved.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+enum insuranceStatus {
+  PENDING,
+  APPROVED
+}
 @Component({
   selector: 'knx-saved',
   styleUrls: ['./saved.component.scss'],
@@ -9,7 +12,7 @@ export class SavedComponent {
   @Input() title: string;
   @Input() insurances: any;
   @Output() onNewAdvice: EventEmitter<any> = new EventEmitter<any>();
-
+  insuranceStatus = insuranceStatus;
   startNewAdvice() {
     this.onNewAdvice.emit();
   }
@@ -26,5 +29,21 @@ export class SavedComponent {
     }
 
     return totalPrice;
+  }
+
+  getInsuranceStatus(insurance): insuranceStatus {
+    let status = this.insuranceStatus.PENDING;
+    switch (insurance.request_status) {
+      case 'pending':
+        status =  this.insuranceStatus.PENDING;
+        break;
+      case 'approved':
+        status =  this.insuranceStatus.APPROVED;
+        break;
+      default:
+        status =  this.insuranceStatus.APPROVED;
+        break;
+    }
+    return status;
   }
 }
