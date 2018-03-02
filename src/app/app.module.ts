8dc9614d59de9aa3a13212a2c0b0d28165291ba6
Matteo <matteo.s@hcl.com>
2018-03-02 11:08:15 +0100

    feat(house-hold): add new env vars for the tags and copies
    Signed-off-by: Matteo <matteo.s@hcl.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, environment.james.langEndpoint);
 }
 
 @NgModule({
