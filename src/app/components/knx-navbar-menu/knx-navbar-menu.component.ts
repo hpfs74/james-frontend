53435f9ac87428bb0a39fb65eea8493f21a5b27b
Mirnes Topalovic <mitopalov@gmail.com>
2018-03-07 14:48:52 +0100

    fix(navbar-menu): clean up code

--- src/app/components/knx-navbar-menu/knx-navbar-menu.component.ts
+++ src/app/components/knx-navbar-menu/knx-navbar-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
-import { slideUpDownHeightAnimation } from '../../shared/animations/slide-up-down.animation';
+import { slideUpDownHeightAnimation } from '@app/shared/animations/slide-up-down.animation';
+import { Profile } from '@app/profile/models';
 
 @Component({
   selector: 'knx-navbar-menu',
@@ -10,5 +11,8 @@ import { slideUpDownHeightAnimation } from '../../shared/animations/slide-up-dow
 })
 export class KNXNavbarMenuComponent {
   @Input() animationState: string;
+  @Input() profile: Profile;
+  @Input() loggedIn: boolean;
   @Output() onAnimationDone: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() onMenuItemClick: EventEmitter<string> = new EventEmitter<string>();
 }
