38a7ae710fdee29d62f77f8b91446a6a56ce8d3b
Mirnes Topalovic <mitopalov@gmail.com>
2018-03-14 10:38:35 +0100

    feat(load-scripts): add csp in local env for faster catching of bugs

--- src/app/core/services/load-scripts.service.ts
+++ src/app/core/services/load-scripts.service.ts
@@ -5,6 +5,9 @@ import { environment } from '@env/environment';
 export class LoadScriptsService {
   constructor() {
     this.initScripts();
+    if (!environment.production) {
+      this.addCSP();
+    }
     // console.log(1);
   }
 
@@ -96,5 +99,151 @@ export class LoadScriptsService {
     document.body.insertBefore(gtmNoScript, document.body.firstChild);
   }
 
+
+  private addCSP() {
+    let meta = document.createElement('meta');
+    meta.httpEquiv = "Content-Security-Policy";
+    meta.content = this.generateCSP();
+    document.head.insertBefore(meta, document.head.firstChild);
+  }
+
+
+  private generateCSP(): string {
+    const contentSecurityPolicy = {
+      "report-uri": "https://report-uri.knab.nl/r/t/csp/enforce",
+      "default-src": [
+        "'unsafe-inline'",
+        "'self'",
+        "https://www.google-analytics.com",
+        "https://www.googletagmanager.com",
+        "http://aegon-middleware-qa.mobgen.com",
+        "https://middleware.test.knabverzekeren.nl",
+        "https://middleware.verzekeren.knab.nl",
+        "https://middleware.uat.knabverzekeren.nl",
+        "https://knab-dev.apigee.net",
+        "https://knab-acc.apigee.net",
+        "https://knab-prd.apigee.net",
+        "https://api.test.knabverzekeren.nl",
+        "https://cdn-static.formisimo.com",
+        "https://tracking.formisimo.com",
+        "https://code.jquery.com"
+      ],
+      "img-src": [
+        "'self'",
+        "data:",
+        "https://ssl.google-analytics.com",
+        "https://www.google.com",
+        "https://www.google.nl",
+        "https://www.google-analytics.com",
+        "https://*.visualwebsiteoptimizer.com",
+        "https://www.facebook.com",
+        "https://googleads.g.doubleclick.net",
+        "https://www.googleadservices.com",
+        "https://secure.adnxs.com",
+        "https://ib.adnxs.com",
+        "https://www.facebook.com/tr/",
+        "https://secure.adnxs.com",
+        "https://www.at19.net",
+        "https://knab.blueconic.net",
+        "https://stats.g.doubleclick.net",
+        "https://ad.doubleclick.net/ddm/activity/src=8163947",
+        "https://secure.adnxs.com",
+        "https://www.google.nl",
+        "https://www.google-analytics.com",
+        "https://stats.g.doubleclick.net",
+        "http://placehold.it",
+        "https://www.google.com/ads",
+        "https://www.gstatic.com",
+        "https://d3cuj82m9z5zxb.cloudfront.net",
+        "http://*.visualwebsiteoptimizer.com",
+        "https://*.cloudfront.net",
+        "http://w.usabilla.com"
+      ],
+      "script-src": [
+        "'unsafe-inline'",
+        "'self'",
+        "'unsafe-eval'",
+        "http://www.googleadservices.com",
+        "https://www.google-analytics.com",
+        "https://www.gstatic.com",
+        "https://www.google.com",
+        "https://ajax.googleapis.com",
+        "https://cdnjs.cloudflare.com",
+        "https://www.google-analytics.com",
+        "https://*.visualwebsiteoptimizer.com",
+        "https://connect.facebook.net",
+        "https://www.googletagmanager.com",
+        "https://ssl.google-analytics.com",
+        "https://www.googleadservices.com",
+        "https://knab.blueconic.net",
+        "https://apis.google.com",
+        "https://googleads.g.doubleclick.net/pagead/viewthroughconversion/837300153",
+        "https://cdn-static.formisimo.com",
+        "http://w.usabilla.com",
+        "http://code.jquery.com",
+        "http://tracking.formisimo.com"
+      ],
+      "style-src": [
+        "'unsafe-inline'",
+        "'self'"
+      ],
+      "frame-src": [
+        "'self'",
+        "https://www.google.com/recaptcha/",
+        "https://*.cobrowse.liveperson.net",
+        "https://lpcdn.lpsnmedia.net",
+        "https://quadia.webtvframework.com",
+        "https://bid.g.doubleclick.net",
+        "https://8163947.fls.doubleclick.net",
+        "https://connect.facebook.net",
+        "https://staticxx.facebook.com/ https://mods.netb.nl"
+      ],
+      "connect-src": [
+        "'self'",
+        "https://knab.blueconic.net",
+
+        "https://www.googletagmanager.com",
+        "https://www.google-analytics.com",
+        "https://middleware.test.knabverzekeren.nl",
+        "https://middleware.verzekeren.knab.nl",
+        "https://middleware.uat.knabverzekeren.nl",
+        "https://knab-dev.apigee.net",
+        "https://knab-acc.apigee.net",
+        "https://knab-prd.apigee.net",
+        "https://api.test.knabverzekeren.nl",
+        "https://d3cuj82m9z5zxb.cloudfront.net",
+        "https://www.google.com/ads/user-lists/837300153",
+        "https://dev.visualwebsiteoptimizer.com/",
+        "https://j58eycphw6.execute-api.eu-west-1.amazonaws.com",
+        "https://cdn-static.formisimo.com",
+        "http://w.usabilla.com",
+        "http://code.jquery.com",
+        "http://tracking.formisimo.com"
+      ],
+      "object-src": [
+        "'none'"
+      ]
+    };
+    if (typeof(contentSecurityPolicy) === "object") {
+
+      return Object.keys(contentSecurityPolicy).reduce( (acc, el) => `${acc} ${this.buildTag(el, contentSecurityPolicy[el])}`, "");
+    }
+    return "";
+  }
+
+  private buildTag(tag, val) {
+    let ret = "";
+    if (val) {
+      if (Array.isArray(val) && val.length > 0) {
+        ret = `${tag} ${val.reduce((acc, el) => acc + " " + el)}; `;
+      }
+
+      if (typeof(val) === "string") {
+        ret = `${tag} ${val}; `;
+      }
+    }
+
+    return ret;
+  }
   /* tslint:enable */
 }
