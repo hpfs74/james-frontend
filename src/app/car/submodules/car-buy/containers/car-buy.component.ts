cfd50645373deead64561df33aa874c05b8494c8
Matteo <matteo.s@hcl.com>
2018-03-30 10:14:15 +0200

    fix(car): Chosen product card on side in desktop during Buy Flow
    Signed-off-by: Matteo <matteo.s@hcl.com>

--- src/app/car/submodules/car-buy/containers/car-buy.component.ts
+++ src/app/car/submodules/car-buy/containers/car-buy.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { AssistantConfig } from '@app/core/models/assistant';
@@ -7,7 +7,7 @@ import { TagsService } from '@app/core/services/tags.service';
 import { QaIdentifier } from '@app/shared/models/qa-identifier';
 import { QaIdentifiers } from '@app/shared/models/qa-identifiers';
 import { KNXWizardStepRxOptions } from '@app/components/knx-wizard-rx/knx-wizard-rx.options';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
 
 import * as fromCar from '@app/car/reducers';
 import * as fromInsurance from '@app/insurance/reducers';
@@ -17,26 +17,37 @@ import * as car from '@app/car/actions/car';
 import * as wizardActions from '@app/core/actions/wizard';
 import { KNXWizardRxService } from '@app/core/services/wizard.service';
 import { SharedService, TEMP_VARIABLE_KEYS } from '@app/shared/services/shared.service';
+import { InsuranceAdvice } from '@insurance/models';
+import { JamesTagPipe } from '@app/shared/pipes';
+import { KNXFinalAdviceOptions } from '@app/components/knx-final-advice/knx-final-advice.options';
 
 @Component({
-  providers: [ AsyncPipe ],
+  providers: [AsyncPipe, JamesTagPipe, CurrencyPipe],
   selector: 'knx-car-buy',
   templateUrl: 'car-buy.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class CarBuyComponent implements OnInit, QaIdentifier {
+export class CarBuyComponent implements OnInit, OnDestroy, QaIdentifier {
   qaRootId = QaIdentifiers.carBuyRoot;
   formSteps: Array<KNXWizardStepRxOptions>;
   chatConfig$: Observable<AssistantConfig>;
   chatMessages$: Observable<Array<ChatMessage>>;
   advice$: Observable<any>;
+  knxFinalAdviceOptions: KNXFinalAdviceOptions;
+  subscription$: Array<any> = [];
+
 
   constructor(private store$: Store<fromCar.State>,
               private tagsService: TagsService,
               public asyncPipe: AsyncPipe,
+              private jamesTag: JamesTagPipe,
+              private currencyPipe: CurrencyPipe,
               public knxWizardService: KNXWizardRxService,
-              public sharedService: SharedService) {}
+              public sharedService: SharedService) {
+
+    this.setFinalAdviceOptions();
+  }
 
   ngOnInit() {
     this.store$.dispatch(new assistant.UpdateConfigAction({
@@ -72,15 +83,56 @@ export class CarBuyComponent implements OnInit, QaIdentifier {
     this.checkFlow();
   }
 
+  ngOnDestroy() {
+    this.subscription$.forEach(sub => sub.unsubscribe());
+  }
+
   goToStep(stepIndex: number) {
     this.store$.dispatch(new wizardActions.Go({stepIndex: stepIndex}));
+  }
+
+  private setFinalAdviceOptions() {
+    this.subscription$.push(
+      this.store$.select(fromInsurance.getSelectedInsurance)
+        .filter(selectedInsurance => !!selectedInsurance)
+        .subscribe((selectedInsurance: InsuranceAdvice) => {
+          this.knxFinalAdviceOptions = {
+            sections: [
+              {
+                logoUrl: selectedInsurance._embedded.insurance.insurance_logo,
+                divider: true
+              },
+              {
+                heading: selectedInsurance['product_name'],
+                key: this.jamesTag.transform(selectedInsurance['main_coverage'], 'car_flow_coverage'),
+                divider: true
+              },
+              {
+                key: 'Eenmalige afsluitkosten',
+                value: this.currencyPipe.transform(selectedInsurance['one_off_premium'], 'EUR', true)
+              },
+              {
+                key: 'Per maand',
+                value: this.currencyPipe.transform(selectedInsurance.monthly_premium, 'EUR', true)
+              }
+            ],
+            button: {
+              text: selectedInsurance.supported ? 'vraag direct aan' : 'Ga naar website',
+              pending: false,
+              onClick: () => {
+              },
+              classes: ['knx-button', 'knx-button--primary', 'knx-button--3d', 'knx-button--float-bottom']
+            }
+          };
+        })
+    );
   }
 
   /**
    * if car flow is set for any reason,
    * for now only used for reset flow,
    * on this point set it back to false, so you can continue as usual
-  */
+   */
   private checkFlow() {
     this.sharedService.tempVariables.set(TEMP_VARIABLE_KEYS.carFlow, false);
   }
