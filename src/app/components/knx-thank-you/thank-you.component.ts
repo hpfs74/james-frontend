933374cd087ad409940c341aea0edeb25ad5bdfb
Alexey Trofimov <alexey.trofimov@hcl.com>
2018-04-05 11:15:36 +0200

    feat(copies): update translations
    Signed-off-by: Alexey Trofimov <alexey.trofimov@hcl.com>

--- src/app/components/knx-thank-you/thank-you.component.ts
+++ src/app/components/knx-thank-you/thank-you.component.ts
@@ -13,30 +13,20 @@ import { Component, Input } from '@angular/core';
             <!-- car specific copy -->
             <div *ngIf="loggedIn">
               <ng-container *ngIf="insuranceType === 'autoverzekering'; else default">
-                <p>We hebben je aanvraag in goede orde ontvangen en gaan hem zo snel mogelijk verwerken bij de verzekeraar.</p>
-                <p>Let op: Je hebt nog niet automatisch een voorlopige dekking als je de ingangsdatum op vandaag hebt gezet.
-                  We doen ons uiterste best om je dezelfde dag nog te activeren.</p>
+                <p>{{'car.purchased.logged_in.text1' | translate}}</p>
+                <p>{{'car.purchased.logged_in.text2' | translate}}</p>
               </ng-container>
-
-              <!--<ng-template #default>-->
-              <!--<p>Gefeliciteerd met je nieuwe {{insuranceType}}verzekering.<br>-->
-              <!--We versturen je aanvraag en de verzekeraar keurt deze binnen 3 dagen goed.-->
-              <!--We houden je hier uiteraard van op de hoogte.</p>-->
-              <!--</ng-template>-->
             </div>
 
             <ol *ngIf="!loggedIn">
-              <li>We hebben je aanvraag in goede orde ontvangen en gaan hem zo snel mogelijk verwerken bij de verzekeraar.
-                Let op: Je hebt nog niet automatisch een voorlopige dekking als je de ingangsdatum op vandaag hebt gezet.
-                We doen ons uiterste best om je dezelfde dag nog te activeren.</li>
-              <li>Klik op <strong>"bevestig account"</strong> in de e-mail die we je net hebben gestuurd om je account
-                in gebruik te nemen. </li>
+              <li>{{'car.purchased.anonymous.text1' | translate}}</li>
+              <li>{{'car.purchased.anonymous.text2' | translate}}</li>
             </ol>
 
-            <p>Zodra je polis definitief is goedgekeurd, krijg je deze van de verzekeraar.</p>
+            <p>{{'car.purchased.note' | translate}}</p>
 
             <p *ngIf="phone && phoneLink">
-              Bij vragen kun je ons direct bereiken via <a href="{{phoneLink}}" rel="noopener">{{phone}}</a>
+              {{'car.purchased.phone' | translate}} <a href="{{phoneLink}}" rel="noopener">{{phone}}</a>
             </p>
           </div>
         </div>
