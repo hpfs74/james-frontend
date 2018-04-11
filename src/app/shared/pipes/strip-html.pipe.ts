6514af44125c62caa67302ad16ef80482442f04c
Matteo <matteo.s@hcl.com>
2018-04-11 21:38:12 +0200

    feat(shared): add strip html from alexey code
    Signed-off-by: Matteo <matteo.s@hcl.com>

---
+++ src/app/shared/pipes/strip-html.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/*
+ * Remove html tags from a string.
+ *
+ * Usage:
+ *   value | stripHtml
+ * Example:
+ *   {{ '<html><body>Test</body></html> | stripHtml }}
+ *   will output: Test
+*/
+@Pipe({name: 'exponentialStrength'})
+export class StripHtmlPipe implements PipeTransform {
+  transform(value: string): string {
+
+    return value.replace(/<\/?[^>]+(>|$)/g, '');
+  }
+}
