780dc58526df77645c199fbcd3aeced8e925d59d
Matteo <matteo.s@hcl.com>
2018-05-01 13:52:59 +0200

    fix(house-hold-buy): fix suggestion in address p1-128
    Signed-off-by: Matteo <matteo.s@hcl.com>

--- src/app/house/submodules/house-hold-premiums/containers/house-hold-premiums-detail/house-hold-premiums-detail.component.ts
+++ src/app/house/submodules/house-hold-premiums/containers/house-hold-premiums-detail/house-hold-premiums-detail.component.ts
@@ -82,7 +82,7 @@ export class HouseHoldPremiumsDetailComponent implements OnInit, OnDestroy {
   }
 
   goToNextStep() {
-    // if (localStorage.getItem('testing')) {
+    if (localStorage.getItem('testing')) {
 
       this.store$.dispatch(new houseHoldAction.NewFlowAdviceStore({
         contacts: {
@@ -103,8 +103,8 @@ export class HouseHoldPremiumsDetailComponent implements OnInit, OnDestroy {
         }
       }));
       this.store$.dispatch(new router.Go({path: ['/inboedel/persoonlijke-gegevens']}));
-    // } else {
-    //   this.store$.dispatch(new layout.OpenModal('houseHoldEndOftheLineModal'));
-    // }
+    } else {
+      this.store$.dispatch(new layout.OpenModal('houseHoldEndOftheLineModal'));
+    }
   }
 }
